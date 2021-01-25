d3c3b9cc9e179e7bb406176ae657e68129bbe8f4
thijshuijbers <thijs@fundament.it>
2021-01-25 11:52:15 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!!!
+          Thijs roeleert de pan uit!
         </p>
         <a
           className="App-link"
